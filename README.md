91b4be07acfe71edecbb6140bfc20d7b0ee882a6
Pig Fang <g-plane@hotmail.com>
2024-01-14 14:13:21 +0800

    add CLI for create or update config

--- README.md
+++ README.md
@@ -4,13 +4,37 @@ Shareable [dprint](https://dprint.dev/) configuration file for Pig Fang's projec
 
 ## Usage
 
+### With CLI
+
+If you have installed Deno, you can use the CLI to create or update the config.
+
+To create config:
+
+```
+deno run --allow-write=. --allow-net https://dprint.gplane.win/cli.ts init
+```
+
+`create` command is an alias for `init`.
+
+To update config:
+
+```
+deno run --allow-read=. --allow-write=. --allow-net https://dprint.gplane.win/cli.ts update
+```
+
+`up` command is an alias for `update`.
+
+### Manually
+
 Edit `dprint.json` like this:
 
 ```json
 {
-  "extends": "https://dprint.gplane.win/dprint.json"
+  "extends": "https://dprint.gplane.win/<version>.json"
 }
 ```
+
+Replace the `<version>` above with actual latest version manually.
 
 ## License
 
---
+++ cli.ts
@@ -0,0 +1,70 @@
+import * as path from 'https://deno.land/std@0.212.0/path/mod.ts'
+import yargs from 'https://deno.land/x/yargs@v17.7.2-deno/deno.ts'
+
+const filePath = path.join(Deno.cwd(), 'dprint.json')
+const baseURL = 'https://dprint.gplane.win'
+
+yargs(Deno.args)
+  .command({
+    command: 'init',
+    aliases: ['create'],
+    description: 'create dprint config',
+    handler: init,
+  })
+  .command({
+    command: 'update',
+    aliases: ['up'],
+    description: 'update to latest config',
+    handler: update,
+  })
+  .strictCommands()
+  .demandCommand(1)
+  .scriptName('cli.ts')
+  .help()
+  .version(false)
+  .parse()
+
+async function init() {
+  const latest = await fetchLatest()
+  if (!latest) {
+    return
+  }
+  await Deno.writeTextFile(
+    filePath,
+    JSON.stringify(
+      { extends: `${baseURL}/${latest.name}.json` },
+      null,
+      2,
+    ),
+  )
+}
+
+async function update() {
+  const config: { extends?: string | string[] } = JSON.parse(
+    await Deno.readTextFile(filePath),
+  )
+
+  const latest = await fetchLatest()
+  if (!latest) {
+    return
+  }
+
+  if (Array.isArray(config.extends)) {
+    const index = config.extends.findIndex((url) => url.startsWith(baseURL))
+    if (~index) {
+      config.extends[index] = `${baseURL}/${latest.name}.json`
+    }
+  } else {
+    config.extends = `${baseURL}/${latest.name}.json`
+  }
+
+  await Deno.writeTextFile(filePath, JSON.stringify(config, null, 2))
+}
+
+async function fetchLatest() {
+  const response = await fetch(
+    'https://api.github.com/repos/g-plane/dprint-config/tags',
+  )
+  const [latest]: Array<{ name: string }> = await response.json()
+  return latest
+}
